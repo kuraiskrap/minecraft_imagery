18850e538d50ff8774d371a1be202975f1460a75
kuraiskrap <56841907+kuraiskrap@users.noreply.github.com>
2024-10-02 09:47:18 -0300

    Update ImageCaptureOptions.java
    disable showing depth as im unsure how to change it within skript

--- src/main/java/dev/jensderuiter/minecraft_imagery/image/ImageCaptureOptions.java
+++ src/main/java/dev/jensderuiter/minecraft_imagery/image/ImageCaptureOptions.java
@@ -47,6 +47,6 @@ public class ImageCaptureOptions {
     private boolean dayLightCycleAware = true;
 
     @Builder.Default
-    private boolean showDepth = true;
+    private boolean showDepth = false;
 
 }
